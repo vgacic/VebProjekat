8dab89235d171b56ee3177a97f82c78222318e52
vgacic <vgacic02@gmail.com>
2023-07-14 15:40:13 +0200

    Kreiranje inicijalnog projekta

---
+++ vebProjekat/src/main/java/com/in51_2021/vebProjekat/VebProjekatApplication.java
@@ -0,0 +1,13 @@
+package com.in51_2021.vebProjekat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VebProjekatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VebProjekatApplication.class, args);
+	}
+
+}
---
+++ vebProjekat/src/test/java/com/in51_2021/vebProjekat/VebProjekatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in51_2021.vebProjekat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VebProjekatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
